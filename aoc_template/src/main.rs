cebb224d645c61b037f9d3cfef52a97f2fec4d5e
Cruxial <benjamin@theones.dk>
2023-12-03 23:50:12 +0100

    Testing cargo-generate

---
+++ aoc_template/src/main.rs
@@ -0,0 +1,19 @@
+use std::env;
+
+mod part01;
+mod part02;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let parameter = &args[2];
+
+    if query == "part" {
+        match parameter.as_str() {
+            "1" => part01::run(),
+            "2" => part02::run(),
+            _ => todo!(),
+        }
+    }
+}
--- aoc_template/src/mod.rs
+++ aoc_template/src/mod.rs
@@ -1,3 +1,2 @@
-pub mod file_helper;
 pub mod part01;
 pub mod part02;
